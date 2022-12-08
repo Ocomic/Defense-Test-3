9ca540fd80f77739e4a874bb2696e7d337c88bec
Ocomic <108867917+Ocomic@users.noreply.github.com>
2022-12-08 23:22:11 +0100

    setting up collider and trigger
    - created collisoin handler script dor late usage of dealing with different collisions

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        
+            Debug.Log(this.name + "collided with " +collision.gameObject.name);
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"{this.name} **Triggered by ** {other.gameObject.name}" );
+    }
+}
