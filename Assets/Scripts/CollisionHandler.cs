d839d6668feb2090e34b044897aeaccc4066187c
Ocomic <108867917+Ocomic@users.noreply.github.com>
2022-12-11 01:46:39 +0100

    Collissions ans Explosions
    - created explosion particle system
    - method for player colliding and exploding with particle effect
    - shoot particles can destroy enemies now

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnCollisionEnter(Collision collision)
-    {
-        
-            Debug.Log(this.name + "collided with " +collision.gameObject.name);
-        
-    }
+    [SerializeField] float delay = 1f;
+    [SerializeField] ParticleSystem crashVFX;
+
+    bool isTransitioning = false;
+    
 
     void OnTriggerEnter(Collider other)
     {
+        StartCrashSequence();
         Debug.Log($"{this.name} **Triggered by ** {other.gameObject.name}" );
     }
+
+    void ReloadLevel()
+    {
+
+        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+
+    }
+
+    void StartCrashSequence()
+    {
+        crashVFX.Play();
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
+        isTransitioning = true;
+        //audioSource.Stop();
+        //crashParticles.Play();
+        //todo add particle effect on crash
+        GetComponent<PlayerControler>().enabled = false;
+        //audioSource.PlayOneShot(crash);
+        Invoke("ReloadLevel", delay);
+
+    }
+
+    
+
 }
